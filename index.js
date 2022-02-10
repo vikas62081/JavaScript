d183caf7bccc01ea6a2a790709bb6dfc35216ccb
Vikas Kumar Vishwakarma <vikas@vikass-MacBook-Air.local>
2022-02-10 12:29:13 +0530

    pushing nested destructuring changes

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-// How to Flatten Array and Object in JavaScript
-
-//input
-const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9, 10]]]]
-const obj = {
+// Nested Object Destructuring in JavaScript
+const person = {
     name: "Vikas", age: 24,
-    address: { flatNo: 32, city: "Chennai", }
+    address: {
+        permanent: { flatNo: 23, city: "Chennai" },
+        temporary: { flatNo: 34, city: "Delhi" }
+    },
+    phone: [987654345, 879846527]
 }
 
-//output
-// arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// obj = { name: "Vikas", age: 24, flatNo: 32, city: "Chennai" }
+person.name//Vikas
+person.age//24
 
-//using javascript function
-// console.log(arr.flat(Infinity)) //arr.flat(1)
-
-const flattenArray = (arr) => {
-    const newArr = [];
-    const getFlattenArr = (arg) => {
-        arg.forEach(dt => {
-            if (Array.isArray(dt)) {
-                getFlattenArr(dt)
-            } else {
-                newArr.push(dt)//pushing new element to array
-            }
-        })
-    }
-    getFlattenArr(arr)
-    return newArr;
-}
-
-
-// console.log(flattenArray(arr))
-
-// console.log(obj)
-const flattenObj = (object) => {
-    const newObj = {}
-    const getFlattenObj = (obj) => {
-        // obj['name']//Vikas
-        Object.keys(obj).forEach(key => {
-            if (typeof obj[key] == "object") {
-                getFlattenObj(obj[key])
-            } else {
-                newObj[key] = obj[key]// adding new property with value
-            }
-        })
-    }
-    getFlattenObj(object)
-    return newObj
-}
-
-
-console.log(flattenObj(obj))
-
-
+const { name: userName, age,
+    address: { permanent: { city: homeTown }, temporary },
+    phone } = person
+// console.log(homeTown, temporary)
+// console.log(userName)
+const [mobileNumber, telephone] = phone
+console.log(mobileNumber, telephone, phone)
