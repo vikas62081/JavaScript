d421c49313dbfd4eb6c837566e44255bdee930bf
vikas62081 <vikas62081@gmail.com>
2022-09-19 12:17:08 +0530

    convert string to caps

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-const data = require("./customer.json");
-console.log(data);
+//How to Uppercase the First Letter in a Word with JS
+//method 1
+
+// const name = "vikas";
+// const uppercase = name.charAt(0).toUpperCase();
+// // const remainingLetters = name.substring(1);
+// const remainingLetters = name.slice(1);
+// const capsWord = uppercase + remainingLetters;
+// console.log(capsWord);
+
+// mothod 2
+
+// const capsWord1 = name.charAt(0).toUpperCase() + name.slice(1);
+// console.log(capsWord1);
+
+// fucntion (word)
+const getCapsString = (str) => {
+  if (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  return null;
+};
+
+// console.log(getCapsString("gdsffgshkjd"));
+
+// for a sentence
+name = "vikas kumar";
+//output Vikas Kumar
+const getCapsSentence = (sentence) =>
+  sentence
+    .split(" ")
+    .map((word) => getCapsString(word))
+    .join(" ");
+
+console.log(getCapsSentence("vikas kumar vishwakarma"));
