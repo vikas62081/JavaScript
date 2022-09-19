721d2600bc789ab43f6b1cdfd94e5f47ce331ab7
vikas62081 <vikas62081@gmail.com>
2022-09-19 11:39:12 +0530

    json reader sample project

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// const evenNumbers = numbers.filter((number, index, array) => number % 2 == 0);
-// console.log(evenNumbers);
-
-Array.prototype.myFilter = function (func) {
-  let filtered = [];
-  for (let i = 0; i < this.length; i++) {
-    if (func(this[i], i, this)) {
-      filtered.push(this[i]);
-    }
-  }
-  return filtered;
-};
-
-// console.log(numbers.myFilter((num) => num % 2 != 0));
-
-const students = [{ name: "Vikas" }, { name: "Gorge" }];
-
-console.log(students.myFilter((stu) => stu.name == "vikas"));
+const data = require("./customer.json");
+console.log(data);
