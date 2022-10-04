4fe01800896ebc11a8d12e167babbe17b5e18137
vikas62081 <vikas62081@gmail.com>
2022-10-04 20:37:45 +0530

    adding db design

---
+++ db.js
@@ -0,0 +1,62 @@
+//category
+// UPSE, SSC,
+const categories = [
+  {
+    id: 1,
+    title: "UPSC",
+    subCategories: [
+      {
+        id: 123,
+        title: "",
+        noOfPapers: 4,
+        post: "post",
+      },
+    ],
+  },
+];
+
+//Question paper card
+[{
+    id: 1,
+        title: "",
+        categoryId: 1,
+        subCategoryId: 2,
+    paperId: '',
+    totalQuestions: 25,
+        date:new Date()
+        
+}]
+////Question paper
+{
+    id: 12, 
+    paperId:"",
+        qTitle: 'What is ur name?',
+        options: [],
+        type: "Single" | "Mutiple",
+        answser:''
+}
+
+
+//after submission
+{
+    userId:''
+    QuestionPaperId
+    QuestionPaperTitle: "",
+    totalQuestions: 12,
+    correctAnswer: 5,
+    timing:''
+}
+//USERs
+
+{
+    id: 1,
+        name: "Vikas",
+        city: "",
+        email: "",//username
+        mobile: "",
+        password: ""//encrypted
+        submitionPapers:[]
+}
+
+
+
